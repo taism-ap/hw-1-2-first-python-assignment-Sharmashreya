65ccbdaba58c15d710cfe4b930de9b2ddf721227
Sharmashreya <54617584+Sharmashreya@users.noreply.github.com>
2019-10-02 11:39:52 +0400

    Update python.py

--- python.py
+++ python.py
@@ -1,10 +1,10 @@
+print("PLEASE, I BEG OF YOU, FOLLOW THE DIRECTIONS. THEY ARE FAIRLY SIMPLE")
 num1 = input("choose your first number ")
 op = str(input("choose 'd' for divide or 'm' for multiply or 'p' for plus or 's' for subtract"))
 num2 = input("choose your second number ")
 
-eq = num1 + op + num2
 
-print("This is the equation you have come up with " + str(eq))
+eq = num1 + op + num2
 
 a = float(num1) + float(num2)
 b = float(num1) / float(num2)
@@ -12,21 +12,25 @@ c = float(num1) * float(num2)
 d = float(num1) - float(num2)
 
 
-if op in ["d"]:
+if op in ["d" ,  "D"]:
     print("Since you chose the operation /, your answer is ")
     print(b)
 
 
-if op in ["m"]:
+elif op in ["m" , "M"]:
     print("Since you chose the operation *, your answer is ")
     print(c)
 
 
-if op in ["p"]:
+elif op in ["p" , "P"]:
     print("Since you chose the operation +, your answer is ")
     print(a)
 
 
-if op in ["s"]:
+elif op in ["s" , "S"]:
     print("Since you chose the operation -, your answer is ")
     print(d)
+
+else:
+    print("FOLLOW THE DIRECTIONS")
+
